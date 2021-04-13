7bd775e10a3a3803f8a98baa19e0b7769e535cb0
HandsomeKoreeZ <alexey-gumen@yandex.ru>
2021-04-13 19:11:45 +0200

    функциональное наполнение.

---
+++ src/main/java/HW2/Client/Client.java
@@ -0,0 +1,24 @@
+package HW2.Client;
+
+import HW2.GUI.ChatFrame;
+
+class Client {
+    private final ChatFrame morda;
+    private final ClientConnection connection;
+    public Client() {
+        connection = new ClientConnection();
+        //подключение графики и отправка сообщений
+        morda = new ChatFrame(connection::sendMessage);
+
+        new Thread(() ->{
+            String message;
+            while (true){
+                //приём сообщений
+                message = connection.receiveMessage();
+                if (message.split("\\s").length>1){
+                    morda.getChatConsumer().accept(message);
+                }
+            }
+        }).start();
+    }
+}
---
+++ src/main/java/HW2/Client/ClientApp2.java
@@ -0,0 +1,7 @@
+package HW2.Client;
+
+public class ClientApp2 {
+    public static void main(String[] args) {
+        new Client();
+    }
+}
